91c4a0619136893457ac57e531e16c469bfb1675
Archisman Banerjee <archisman.tech@gmail.com>
2025-07-17 19:56:21 +0530

    Initial Commit

---
+++ Enroute SwiftUI/Enroute_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enroute_SwiftUIApp.swift
+//  Enroute SwiftUI
+//
+//  Created by Archisman on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Enroute_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
